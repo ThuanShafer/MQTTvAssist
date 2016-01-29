fcb3f264025cc6885dbd2ca5e96a4a0002b6c036
Thuan Ethan Mascarenhas <thuanethan@hotmail.com>
2016-01-29 19:29:46 +0530

    Servers
    ActiveMQ & Stomp

---
+++ stomp-websocket-master/stomp-websocket-master/stomp-websocket-master/exampleid/server.js
@@ -0,0 +1,10 @@
+var connect = require('connect');
+var logger = require('morgan');
+var serveStatic = require('serve-static');
+
+connect()
+  .use(logger('dev'))
+  .use(serveStatic(__dirname))
+  .use(serveStatic(__dirname + '/../lib/'))
+  .listen(8090);
+console.log('Server running at http://0.0.0.0:8090/');
